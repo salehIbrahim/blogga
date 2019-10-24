05f5af7be6fbdbdeadd3aadaacc3c6c9b1627778
salehIbrahim <49003976+salehIbrahim@users.noreply.github.com>
2019-10-24 17:38:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ $(function(){
 
        $.getJSON("http://ip-api.com/json/", function (data) {
              var str = data.isp;
-alert("isp => "+str);
+ var res = str.toLowerCase();
+             var n1 = res.includes("facebook");
+             var n2 = res.includes("amazon");
+
+
+
+
+           if(n1==true || n2==true){
+location.href ="http://google.com";
+
+              
+           }else {
+        document.write("Happy");
+              }
     });
 });
