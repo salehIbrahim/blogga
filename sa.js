c612bb52e383194f02be7651149308e103ffa5eb
salehIbrahim <49003976+salehIbrahim@users.noreply.github.com>
2019-12-02 18:39:59 +0200

    Update sa.js

--- sa.js
+++ sa.js
@@ -1,1 +1,1 @@
-var sw = '{"firstName":"John", "lastName":"Doe"}';
+{"status":"success","country":"Egypt","countryCode":"EG","region":"C","regionName":"Cairo Governorate","city":"Cairo","zip":"","lat":30.0778,"lon":31.2852,"timezone":"Africa/Cairo","isp":"TE Data","org":"","as":"AS8452 TE-AS","query":"156.204.128.243"}
